52aeaf1ac26db8276b231ad706bb89e23b22a296
zhengzhuang <zhengzhuang@yuanduyun.com>
2023-08-17 16:38:55 +0800

    feat: 新增低代码组件

---
+++ src/components/Lowcode/index.tsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: {zhengzhuang}
+ * @Date: 2023-08-17 16:37:31
+ * @LastEditors: {zhengzhuang}
+ * @LastEditTime: 2023-08-17 16:38:09
+ * @Description: 
+ */
+import * as React from "react";
+
+const Index: React.FC = () => {
+  return (
+    <>
+      <p>Index IndexIndexIndex</p>
+    </>
+  );
+};
+
+export default Index;
--- src/pages/lowcode/index.tsx
+++ src/pages/lowcode/index.tsx
@@ -2,15 +2,16 @@
  * @Author: {zhengzhuang}
  * @Date: 2023-08-17 16:29:08
  * @LastEditors: {zhengzhuang}
- * @LastEditTime: 2023-08-17 16:29:18
- * @Description: 
+ * @LastEditTime: 2023-08-17 16:38:29
+ * @Description:
  */
-import * as React from "react";
+import * as React from 'react';
+import Lowcode from '../../components/Lowcode';
 
 const Index: React.FC = () => {
   return (
     <>
-      <p>Index IndexIndexIndex</p>
+      <Lowcode />
     </>
   );
 };
